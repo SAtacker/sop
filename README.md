39613831fe9da48ad899d9ad0d47b3ecfe35e51f
Shreyas Atre <shreyasatre16@gmail.com>
2022-11-18 21:39:26 +0530

    Added generated images using pdftoppm
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ tlmgr init-usertree
 ```
 chmod +x compile_on_modify.sh
 ```
+
+![SoP](./statement_of_purpose-1.png)
+![SoP](./statement_of_purpose-2.png)
--- compile_on_modify.sh
+++ compile_on_modify.sh
@@ -10,9 +10,17 @@ if ! which xelatex >/dev/null; then
     exit 1
 fi
 
+if ! which pdftoppm >/dev/null; then
+    echo "Install poppler-utils"
+    exit 1
+fi
+
 xelatex statement_of_purpose.tex --quiet
+pdftoppm statement_of_purpose.pdf statement_of_purpose -png
+
 inotifywait -r -m -e modify statement_of_purpose.tex |
     while read -r file_path file_event file_name; do
         echo "${file_path}${file_name} event: ${file_event}"
         xelatex statement_of_purpose.tex --quiet
+        pdftoppm statement_of_purpose.pdf statement_of_purpose -png
     done
